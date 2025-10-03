f5ae85539458e0036f91fd24bbbf01a8e0509ab2
Daniel <perdoher@hotmail.com>
2025-10-03 16:50:15 -0500

    Adds Java syntax and data types learning examples Introduces basic Java syntax and data type demonstrations with explanatory comments and output to aid beginners in understanding variables, operators, conditionals, loops, and both primitive and non-primitive types. Facilitates learning by example for those new to Java programming.

---
+++ OperadoresComparativos.java
@@ -0,0 +1,14 @@
+public class OperadoresComparativos {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+
+        // Operadores de comparación
+        System.out.println("a es igual a b: " + (a == b));
+        System.out.println("a es diferente de b: " + (a != b));
+        System.out.println("a es mayor que b: " + (a > b));
+        System.out.println("a es menor que b: " + (a < b));
+        System.out.println("a es mayor o igual que b: " + (a >= b));
+        System.out.println("a es menor o igual que b: " + (a <= b));
+    }
+}
